f3cb01c78c2d10d1e13ed29738a8ace9e5040e87
annasnurdin <anasnurdin02@gmail.com>
2025-11-14 14:13:09 +0700

    using live json-server

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+// const api = axios.create({
+//   baseURL: "http://localhost:3000",
+// });
+
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://json-simple-lms.vercel.app",
 });
 
 // const api = axios.create({
